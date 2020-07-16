ea29f2874e45d7668eb8d45205e0688351a41127
hjyeon-n <hyunjy25@gmail.com>
2020-07-16 23:53:45 +0900

    11727 solution

---
+++ BOJ/2020.07/Solution_11727/11727_top-down.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class 11727_top-down {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner scan = new Scanner(System.in);
+				
+		int num = scan.nextInt();
+		int[] memo = new int[num + 1];
+		
+		memo[1] = 1;
+		
+		if (num > 1) {
+			memo[2] = 3;
+			for (int i = 3; i <= num; i++) {
+				memo[i] = -1;
+			}
+		}
+		
+		System.out.println(tile(num, memo));
+	}
+	
+	public static int tile(int n, int[] memo) {
+		if (n == 1) {
+			return 1;
+		}
+		if(memo[n] != -1) {
+			return memo[n];
+		}
+		else {
+			memo[n] = (tile(n - 1, memo) + 2 * tile(n - 2, memo)) % 10007; 
+			return memo[n];
+		}
+	}
+}
