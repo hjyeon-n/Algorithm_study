b5db3610daab8bbdae3aecc11e64ab6f415ccfe2
hjyeon-n <hyunjy25@gmail.com>
2020-07-28 17:16:36 +0900

    CyclicRotation solution

---
+++ Codility/2020.07/CyclicRotation.java
@@ -0,0 +1,22 @@
+public class Main {
+	public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+		
+		if (A.length == 0) {
+			return A;
+		}
+		
+		if (K == 0 || K == A.length || K % A.length == 0) {
+			return A;
+		}
+		
+		for (int i = 0; i < K; i++) {
+			int tmp = A[A.length - 1];
+			for (int j = A.length - 2; j >= 0; j--) {
+				A[j + 1] = A[j];
+			}
+			A[0] = tmp;
+		}
+		return A;
+    }
+}
