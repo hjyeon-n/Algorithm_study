e3467f879939061db27d03a983fbbcf6d6982ea7
hjyeon-n <hyunjy25@gmail.com>
2020-07-31 16:15:24 +0900

    11055 solution

---
+++ BOJ/2020.07/Solution_11055.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class Solution_11055 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		int arr[] = new int[num + 1];
+		int dp[] = new int[num + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= num; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[0] = 0;
+		for (int i = 1; i <= num; i++) {
+			int idx = 0;
+			for (int j = 1; j < i; j++) {
+				if (arr[i] > arr[j] && dp[idx] < dp[j]) {
+					idx = j;
+				}
+			}
+			dp[i] = dp[idx] + arr[i];
+		}
+		Arrays.sort(dp);
+		System.out.println(dp[num]);
+	}
+}
