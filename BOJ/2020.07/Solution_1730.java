c30e631498683a0ba54f010c3891985b7eba4824
hjyeon-n <hyunjy25@gmail.com>
2020-07-31 13:40:47 +0900

    Rename class

--- BOJ/2020.07/Solution_1730.java
+++ BOJ/2020.07/Solution_1730.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Solution_1730 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
