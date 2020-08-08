ce281d54dece34dc703c66d4800eab1e89a58baf
hjyeon-n <hyunjy25@gmail.com>
2020-08-09 00:51:46 +0900

    1904 solution

---
+++ BOJ/2020.08/Solution_1904.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[num + 1];
+		
+		dp[1] = 1;
+		
+		if (num > 1) {
+			dp[2] = 2;
+		}
+		
+		for (int i = 3; i <= num; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+		}
+		
+		System.out.println(dp[num]);
+	}
+}
