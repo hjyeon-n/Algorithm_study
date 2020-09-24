63dee580f84d45effe3b618ffc2824319e543625
hjyeon-n <hyunjy25@gmail.com>
2020-09-25 00:12:02 +0900

    완주하지 못한 선수 solution

---
+++ Programmers/2020.09/완주하지 못한 선수/완주하지 못한 선수_ver2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Programmers {
+	static ArrayList<Integer> preorder;
+	static ArrayList<Integer> postorder;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] participant = {"mislav", "stanko", "mislav", "ana"};
+		String[] completion = {"stanko", "ana", "mislav"};
+		solution(participant, completion);
+	}
+	
+	public static String solution(String[] participant, String[] completion) {
+		String answer = "";
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+        
+        for (int i = 0; i < participant.length; i++) {
+            if (map.get(participant[i]) == 1) {
+                answer = participant[i];
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
