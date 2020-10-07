3768c3f75aca0d67063bc0c64837692da5677448
hjyeon-n <hyunjy25@gmail.com>
2020-10-07 17:03:39 +0900

    Update 체육복 solution

--- Programmers/2020.10/체육복/체육복_ver2.java
+++ Programmers/2020.10/체육복/체육복_ver2.java
@@ -10,18 +10,21 @@ public class Programmers {
     	int[] people = new int[n];
         int answer = n;
 
-        for (int l : lost) 
+        for (int l : lost) {
             people[l - 1]--;
-        for (int r : reserve) 
+        }
+        
+        for (int r : reserve) {
             people[r - 1]++;
+        }
 
-        for (int i = 0; i < people.length; i++) {
+        for (int i = 0; i < n; i++) {
             if(people[i] == -1) {
                 if(i - 1 >= 0 && people[i - 1] == 1) {
                     people[i]++;
                     people[i - 1]--;
                 }
-                else if(i + 1 < people.length && people[i + 1] == 1) {
+                else if(i + 1 < n && people[i + 1] == 1) {
                     people[i]++;
                     people[i+1]--;
                 }
