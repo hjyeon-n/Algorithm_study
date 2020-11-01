12f0457c20e240e7255dea9f2224dcb3fb075a0e
hjyeon-n <hyunjy25@gmail.com>
2020-11-01 16:15:00 +0900

    2004 solution

---
+++ BOJ/2020.10/Solution_2004.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int twoCnt = getCnt(n, 2) - getCnt(m, 2) - getCnt(n - m, 2);
+        int fiveCnt = getCnt(n, 5) - getCnt(m, 5) - getCnt(n - m, 5);
+        
+        System.out.println(Math.min(twoCnt, fiveCnt));
+	}
+
+	public static int getCnt(int n, int k) {
+	    int cnt = 0;
+	    
+	    while(n >= k) {
+	        cnt += n / k;
+	        n /= k;
+	    }
+	    
+	    return cnt;
+	}
+}
