762083ebb361a4c345b7027cf62778d120b24009
hjyeon-n <hyunjy25@gmail.com>
2020-11-10 17:43:37 +0900

    9466 solution

---
+++ BOJ/2020.11/Solution_9466.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_9466 {
+	static int[] arr;
+	static boolean[] visited;
+	static boolean[] finished;
+	static int cnt;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+			arr = new int[n + 1];
+			visited = new boolean[n + 1];
+			finished = new boolean[n + 1];
+			cnt = 0;
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= n; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+			}
+			
+			for (int j = 1; j <= n; j++) {
+				dfs(j);
+			}
+			
+			System.out.println(n - cnt);
+		}
+	}
+	
+	public static void dfs(int now) {
+		if (visited[now]) {
+            return;
+		}
+ 
+        visited[now] = true;
+        int next = arr[now];
+ 
+        if (!visited[next]) {
+            dfs(next);
+        }
+        else {
+            if (!finished[next]) {
+                cnt++;
+                for (int i = next; i != now; i = arr[i]) {
+                    cnt++;
+                }
+            }
+        }
+ 
+        finished[now] = true;
+	}
+}
