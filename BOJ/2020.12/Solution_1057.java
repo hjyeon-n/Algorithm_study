6e212c448a5cb97a25581b5f794d3ecbb0aa7cc6
hjyeon-n <hyunjy25@gmail.com>
2020-12-17 22:35:52 +0900

    1057 solution

---
+++ BOJ/2020.12/Solution_1057.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken()) - 1;
+		int L = Integer.parseInt(st.nextToken()) - 1;
+		
+		int K_n = K;
+		int L_n = L;
+		int round = 1;
+		
+		while (true) {
+			K_n /= 2;
+			L_n /= 2;
+			
+			if (K_n == L_n) {
+				System.out.println(round);
+				break;
+			}
+			
+			if (K_n % 2 == 0 && L_n - K_n == 1) {
+				System.out.println(round + 1);
+				break;
+			}
+			round++;
+		}
+		
+		
+	}
+}
