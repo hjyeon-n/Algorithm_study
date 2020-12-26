efc43b2a722de3e01ee079731c382e9a7f0c26e9
hjyeon-n <hyunjy25@gmail.com>
2020-12-26 23:10:56 +0900

    Rename file

--- BOJ/2020.12/Solution_3184.java
+++ BOJ/2020.12/Solution_3184.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Solution_2184 {
+public class Solution_3184 {
 	static char[][] map;
 	static boolean[][] visited;
 	static int R, C;
