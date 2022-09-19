64611096d591ab4d409ff0bf643da4cc690d1ff7
hjyeon-n <hyunjy25@gmail.com>
2022-09-19 22:27:50 +0900

    없는 숫자 더하기 solution

---
+++ Programmers/2022.09/없는 숫자 더하기.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		int[] numbers = {1,2,3,4,6,7,8,0};
+		System.out.println(solution(numbers));
+	}
+
+	 public static int solution(int[] numbers) {
+	        int answer = 45;
+	        
+	        for (int i = 0; i < numbers.length; i++) {
+	            answer -= numbers[i];
+	        }
+	        
+	        return answer;      
+	 }
+}
